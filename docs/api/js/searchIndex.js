4da947d9cc9030bb9d22a5529d4870c300731504
riandomt <mittou.dorian@gmail.com>
2024-10-17 10:20:23 +0200

    dossier de base

---
+++ docs/api/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\bernard\u0028\u0029",
+            "name": "bernard",
+            "summary": "Undocumented\u0020function",
+            "url": "namespaces/default.html#function_bernard"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
